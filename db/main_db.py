1799ba3cea3c5798ee466b054d2c6286f7672f65
Kurmanbek-1 <kurmanbekmomunkulov32@gmail.com>
2026-01-16 18:50:28 +0600

    Lesson 6

--- db/main_db.py
+++ db/main_db.py
@@ -9,3 +9,22 @@ def init_db():
     cursor.execute(queries.task_table)
     conn.commit()
     conn.close()
+
+
+def add_task(task):
+    conn = sqlite3.connect(path_db)
+    cursor = conn.cursor()
+    cursor.execute(queries.insert_task, (task, ))
+    conn.commit()
+    task_id = cursor.lastrowid
+    conn.close()
+    return task_id 
+
+
+def update_task(task_id, new_task):
+    conn = sqlite3.connect(path_db)
+    cursor = conn.cursor()
+    cursor.execute(queries.update_task, (new_task, task_id))
+    conn.commit()
+    conn.close()
+
