d261c02db908eab6a1b0a6e7f1d970710de402f8
Kurmanbek-1 <kurmanbekmomunkulov32@gmail.com>
2026-01-22 01:44:47 +0600

    lesson 7

--- main.py
+++ main.py
@@ -8,8 +8,17 @@ def main(page: ft.Page):
 
     task_list = ft.Column(spacing=25)
 
-    def view_tasks(task_id, task_text):
+    filter_type = 'all'
+
+    def load_tasks():
+        task_list.controls.clear()
+        for task_id, task_text, completed in main_db.get_tasks(filter_type):
+            task_list.controls.append(view_tasks(task_id=task_id, task_text=task_text, completed=completed))
+
+    def view_tasks(task_id, task_text, completed=None):
         task_field = ft.TextField(read_only=True, value=task_text, expand=True)
+
+        checkbox = ft.Checkbox(value=bool(completed), on_change=lambda e: toggle_task(task_id=task_id, is_completed=e.control.value))
 
         def enable_edit(_):
             if task_field.read_only == True:
@@ -25,7 +34,12 @@ def main(page: ft.Page):
 
         save_button = ft.IconButton(icon=ft.Icons.SAVE, on_click=save_task)
 
-        return ft.Row([task_field, edit_button, save_button])
+        return ft.Row([checkbox, task_field, edit_button, save_button])
+    
+    def toggle_task(task_id, is_completed):
+        print(is_completed)
+        main_db.update_task(task_id=task_id, completed=int(is_completed))
+        load_tasks()
 
     def add_task_db(_):
         if task_input.value:
@@ -41,9 +55,21 @@ def main(page: ft.Page):
     task_input = ft.TextField(label='Введите задание:', expand=True, on_submit=add_task_db)
     task_add_button = ft.IconButton(icon=ft.Icons.ADD, on_click=add_task_db)
 
+    def set_filter(filter_value):
+        nonlocal filter_type
+        filter_type = filter_value
+        load_tasks()
+
+    filter_buttons = ft.Row([
+        ft.ElevatedButton('Все задачи', on_click=lambda e: set_filter('all'), icon=ft.Icons.ALL_INBOX),
+        ft.ElevatedButton('В работе', on_click=lambda e: set_filter('uncompleted'), icon=ft.Icons.WATCH_LATER),
+        ft.ElevatedButton('Готово', on_click=lambda e: set_filter('completed'), icon=ft.Icons.CHECK_BOX)
+    ], alignment=ft.MainAxisAlignment.SPACE_EVENLY)
+
     input_row = ft.Row([task_input, task_add_button])
 
-    page.add(input_row, task_list)
+    page.add(input_row, filter_buttons, task_list)
+    load_tasks()
 
 
 if __name__ == '__main__':
